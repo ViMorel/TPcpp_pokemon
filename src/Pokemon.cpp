9f8d5e77bfeafe8cf3de514936e3507ffd004dbf
morelv <victormorel45190@gmail.com>
2025-09-13 17:19:07 +0200

    test

---
+++ src/Pokemon.cpp
@@ -0,0 +1,25 @@
+#include "Pokemon.hpp"
+#include <iostream>
+
+using namespace std;
+
+void Pokemon::sethitpoint(double damage){
+    if(hitpoint > 0){
+        this->hitpoint = hitpoint - damage;
+    }
+};
+
+void Pokemon::attackother(Pokemon &defender){
+    double atk = 0;
+    double def = 0;
+    double hp = 100;
+    atk = this->attack;
+    def = defender.getdefense();
+    hp = defender.gethitpoint();
+    if(hp <= 0){
+        return;
+    };
+    if(atk > def){
+        defender.sethitpoint(atk - def);
+    };
+};
---
+++ src/Pokemon.hpp
@@ -0,0 +1,46 @@
+#include <string>
+
+using namespace std;
+
+class Pokemon {
+    private:
+        int id;
+        string name;
+        double hitpoint;
+        double attack;
+        double defense;
+        int generation;
+
+    public:
+        static int counter;
+
+        Pokemon(const int id,
+                const string name,
+                const int evolution,
+                const double max_hitpoint,
+                double hitpoint,
+                double attack,
+                double defense,
+                const int generation);
+        
+        Pokemon(const Pokemon &poke){
+            id = poke.id;
+            name = poke.name;
+            hitpoint = poke.hitpoint;
+            attack = poke.attack;
+            defense = poke.defense;
+            generation = poke.generation;
+        };
+
+        void attackother(Pokemon &defender);
+
+        double getattack(){return attack;};
+        double getdefense(){return defense;};
+        double gethitpoint(){return hitpoint;};
+
+        void sethitpoint(double damage);
+
+        void display_info() const{};
+
+        ~Pokemon(){};
+};
